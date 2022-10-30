820b271cf3967432164d2a80d3ef08c3abc82ff2
ph1ll <ph1ll@users.noreply.github.com>
2022-10-30 09:48:47 +0000

    Initial commit

---
+++ src/Freyja/Configuration/Features.cs
@@ -0,0 +1,8 @@
+namespace Freyja.Configuration;
+
+public class Features
+{
+    public bool WebApi { get; set; } = true;
+    public bool WebUi { get; set; } = true;
+    public bool OrleansSilo { get; set; } = true;
+}
---
+++ src/Freyja/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Freyja;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Freyja.Configuration;
+
+public class Program
+{
+    private static Features features = new();
+
+    public static async Task Main(string[] args)
+    {
+        var hostBuilder = Host.CreateDefaultBuilder(args);
+
+        hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.Configure<Features>(context.Configuration.GetSection(nameof(Features)));
+            features = services.BuildServiceProvider().GetService<IOptions<Features>>()?.Value ?? new();
+        });
+
+        if (features.WebApi || features.WebUi)
+        {
+            hostBuilder.ConfigureWebHostDefaults(webHostBuilder =>
+            {
+                if (features.WebUi)
+                {
+                    webHostBuilder.Configure((context, app) =>
+                    {
+                        app.UseStaticFiles();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapFallbackToFile("index.html");
+                        });
+                    });
+                }
+            });
+        }
+
+        await hostBuilder.Build().RunAsync();
+    }
+}
+
+
